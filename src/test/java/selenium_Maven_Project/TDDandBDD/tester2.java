27ccedcb4b15a4c212f2a86e6e60c27b27abc828
manishagodase <godase.17@gmail.com>
2024-02-13 07:21:28 -0500

    adde

---
+++ src/test/java/selenium_Maven_Project/TDDandBDD/tester2.java
@@ -0,0 +1,28 @@
+package selenium_Maven_Project.TDDandBDD;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class tester2 {
+
+	WebDriver driver;
+	@Test
+	public void demo() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("http://localhost:8888/index.php?action=Login&module=Users");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+		
+		Thread.sleep(4000);
+		Assert.assertEquals(driver.getCurrentUrl(), "http://localhost:8888/index.php?action=index&module=Home");
+		System.out.println("Done tester2");
+	}
+}
